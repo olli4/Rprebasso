5fcaf317ee63487e2a71c1d62b1935484510c9c2
olli4 <Olli.Niemitalo@hamk.fi>
2025-02-03 17:17:23 +0200

    multiply PAR by factor before minval (attempt to fix WebR bug)

--- src/A_routines.f90
+++ src/A_routines.f90
@@ -2371,6 +2371,7 @@ subroutine minFaparCalc(fAPARtrees,nYears,minFapar,fAparFactor)
    integer :: lastYears = 5 !lastYears are the number of years before the current years that should not be considered in the minimum fAPAR calculations
    integer :: maxYears =15 !maxYears are the total number of years before the current years that should be considered in the minimum fAPAR calculations
    integer :: firstYear
+   real (kind=8) :: tempArray(nYears)  ! Temporary storage for minval
    
    if(nYears <= lastYears) minFapar = fAPARtrees(1)*fAparFactor
    if(nYears > lastYears .and. nYears <= int((maxYears - lastYears)/2)) minFapar = minval(fAPARtrees(1:(nYears-5)))*fAparFactor
@@ -2378,8 +2379,10 @@ subroutine minFaparCalc(fAPARtrees,nYears,minFapar,fAparFactor)
    if((nYears-maxYears+1) < 0) then
     firstYear=1
     ! Print length of fAPARtrees, firstYear, and nYears-5
-    print *, "Length of fAPARtrees: ", size(fAPARtrees), ", firstYear: ", firstYear, ", nYears-5: ", nYears-5
-    minFapar = minval(fAPARtrees(firstYear:(nYears-5)))*fAparFactor
+    ! Length of fAPARtrees:  6 , firstYear:  1 , nYears-5:  1  --->  this will give fAPARtrees(1:1)
+    !print *, "Length of fAPARtrees: ", size(fAPARtrees), ", firstYear: ", firstYear, ", nYears-5: ", nYears-5
+    tempArray(firstYear:(nYears-5)) = fAPARtrees(firstYear:(nYears-5))*fAparFactor
+    minFapar = minval(tempArray(firstYear:(nYears-5)))
    else
     firstYear = nYears-maxYears+1
     minFapar = minval(fAPARtrees(firstYear:(nYears-5)))  
